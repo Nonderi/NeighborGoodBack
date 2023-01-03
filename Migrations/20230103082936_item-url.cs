531b0fe997d2426d4ba9c703efa7b5d60355d780
Jarmo Lahti <jarmo.t.lahti@gmail.com>
2023-01-03 11:34:12 +0200

    Item muutoksia

---
+++ Migrations/20230103082936_item-url.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeighborGoodAPI.Migrations
+{
+    public partial class itemurl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageName",
+                table: "Items",
+                newName: "ImageUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Items",
+                newName: "ImageName");
+        }
+    }
+}
